1abf6788bf52401c339fb71100fd82889c0974b3
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-09 13:11:40 +0900

    register route to server

--- src/swish.rs
+++ src/swish.rs
@@ -1,16 +1,22 @@
 use crate::router;
 use crate::matcher;
 
-use crate::router::Router;
+use crate::router::{Router, Handler};
 
 pub struct Swish {
     pub router: Router,
 }
 
-pub fn swish() -> Swish {
-    Swish {
-        router: Router {
-            routes: Vec::new(),
+impl Swish {
+    pub fn new() -> Swish {
+        Swish {
+            router: Router {
+                routes: Vec::new(),
+            }
         }
+    }
+
+    pub fn swish(&mut self, path: &str, handler: Handler) {
+        self.router.register(path, handler)
     }
 }
