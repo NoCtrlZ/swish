b44a2364ea3609e051f39b412567e92f988e5741
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-09 13:46:57 +0900

    successful get http request

--- src/parser.rs
+++ src/parser.rs
@@ -1,22 +1,17 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 
-#[derive(Debug)]
-pub struct Request {
-    pub path: String,
-    pub body: String,
-}
-
-pub fn parse(stream: &mut TcpStream) {
+pub fn parse(stream: &mut TcpStream) -> String {
     let raw_data = convert(stream);
     println!("{:?}", raw_data);
+    raw_data
 }
 
 fn convert(stream: &mut TcpStream) -> String {
     // content type length limitation is 5000
     let mut buffer = [0; 5000];
     stream.read(&mut buffer).expect("fail to read buffer from stream");
-    println!("{:?}", &buffer[..]);
+    // println!("{:?}", &buffer[..]);
     String::from_utf8_lossy(&buffer[..])
         .trim_matches(char::from(0))
         .to_string()
