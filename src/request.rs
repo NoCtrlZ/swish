d62f069b5f1ae0b899caa7ad5d6cdb62bd81dfe5
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-09 17:45:07 +0900

    remove parser

--- src/request.rs
+++ src/request.rs
@@ -1,5 +1,33 @@
+use std::net::TcpStream;
+use crate::entities::convert_buffer_to_string;
+
 #[derive(Debug)]
 pub struct Request {
     pub method: String,
     pub path: String,
+}
+
+pub fn parse(stream: &mut TcpStream) -> Request {
+    let raw_data = convert_buffer_to_string(stream);
+    // println!("{:?}", raw_data);
+    let request = convert_string_to_request(&raw_data);
+    // println!("{:?}", request);
+    request
+}
+
+
+fn convert_string_to_request(text: &str) -> Request {
+    let mut components = text.split_whitespace();
+    let method = match components.nth(0) {
+        Some(e) => e,
+        None => "GET",
+    };
+    let path = match components.nth(0) {
+        Some(e) => e,
+        None => "/",
+    };
+    Request {
+        method: method.to_string(),
+        path: path.to_string(),
+    }
 }
