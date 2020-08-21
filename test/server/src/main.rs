30a03b69c2eeece420ce47572c075d3f80ac44d4
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-21 12:51:40 +0900

    successful import swish crate

--- test/server/src/main.rs
+++ test/server/src/main.rs
@@ -1,8 +1,6 @@
 extern crate swish_swish;
 
-use swish_swish::*;
-
 fn main() {
-    let mut swish = swish_swish::Swish::new();
+    let swish = swish_swish::Swish::new();
     println!("Hello, world!");
 }
