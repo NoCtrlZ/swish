c20851b1ad5921a912f196be8b729e02437b10ad
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-24 23:25:05 +0900

    list up test types

--- test/server/src/main.rs
+++ test/server/src/main.rs
@@ -1,20 +1,3 @@
-extern crate swish_swish;
-
-mod sample;
-
-use swish_swish::*;
-use sample::{path_handler, user_id_handler, user_register_handler};
-
-fn swish_swish() -> Swish {
-    let mut swish = Swish::new();
-    swish.get("/path", path_handler);
-    swish.get("/user/:id", user_id_handler);
-    swish.post("/user/register", user_register_handler);
-    swish.set_cors_as(allow_everything());
-    swish.set_address("0.0.0.0");
-    swish
-}
-
 fn main() {
-    swish_swish().bish();
+    println!("Hello World!");
 }
