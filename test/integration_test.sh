62e7e9d52dcaadc1c4ee24f26a60821e02efa972
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-24 23:25:05 +0900

    list up test types

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-./server/target/debug/test &
-(cd client && npm run test normal.test.js)
-ps aux | grep ./server/target/debug/test | grep -v grep | ask '{ print "kill -9", $1 }' | sh
-(cd server && cp src/cors_test.rs src/main.rs && cargo build)
-./server/target/debug/test &
-(cd client && npm run test cors.test.js)
-# exit 0
+declare -a Tests=("normal" "cors")
+
+i=0
+for test in ${Test[*]}; do
+    ps aux | grep ./server/target/debug/test | grep -v grep | ask '{ print "kill -9", $1 }' | sh
+    (cd server && cp src/${test}_test.rs src/main.rs && cargo build)
+    ./server/target/debug/test &
+    (cd client && npm run test ${test}.test.js)
+    let i++
+done
+
+exit 0
