3a51ba49f29c1723605ae0e245569027d257a8b9
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-25 00:04:16 +0900

    change array style

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-Tests=("normal" "cors")
+set -- "normal" "cors"
 
-i=0
-for test in ${Test[*]}; do
-    ps aux | grep ./server/target/debug/test | grep -v grep | ask '{ print "kill -9", $1 }' | sh
-    (cd server && cp src/${test}_test.rs src/main.rs && cargo build)
+while [ $# -gt 0 ]
+do
+    ps aux | grep ./server/target/debug/test | grep -v grep | awk '{ print "kill -9", $1 }' | sh
+    (cd server && cp src/$1_test.rs src/main.rs && cargo build)
     ./server/target/debug/test &
-    (cd client && npm run test ${test}.test.js)
-    let i++
+    (cd client && npm run test $1.test.js)
+    shift
 done
 
 exit 0
