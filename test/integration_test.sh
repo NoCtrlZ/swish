4dee8bb9415be3205a6fb3f08963040076d0e4c5
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-24 23:14:15 +0900

    kill server process

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -2,6 +2,7 @@
 
 ./server/target/debug/test &
 (cd client && npm run test normal.test.js)
+ps aux | grep ./server/target/debug/test | grep -v grep | ask '{ print "kill -9", $1 }' | sh
 (cd server && cp src/cors_test.rs src/main.rs && cargo build)
 ./server/target/debug/test &
 (cd client && npm run test cors.test.js)
