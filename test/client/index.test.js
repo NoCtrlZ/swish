ca81984171dc1439f7f2e8f7e627d106457cbbfc
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-24 18:32:11 +0900

    integration test setup

--- test/client/index.test.js
+++ test/client/index.test.js
@@ -2,20 +2,38 @@ const Client = require('hear_me_roar').default
 const client = new Client('localhost', 3000)
 
 describe("Swish Swish Integration Test", () => {
-    it("Normal Get Request", () => {
+    it("Static Get Request", () => {
         client.get('/path').then(res => {
             const { status, data } = res
+
             expect(status).toStrictEqual(true)
+            expect(data.code).toStrictEqual(200)
+            expect(data.data).toStrictEqual("path request")
         })
     })
+
+    it("Dynamic Get Request", () => {
+        client.get('/user/shinsaku').then(res => {
+            const { status, data } = res
+
+            expect(status).toStrictEqual(true)
+            expect(data.code).toStrictEqual(200)
+            expect(data.data).toStrictEqual("user id is shinsaku")
+        })
+    })
+
     it("Normal Post Request", () => {
         const sample = {
             code: 200,
             data: 'hello'
         };
+
         client.post('/user/register', sample).then((res) => {
             const { status, data } = res
+
             expect(status).toStrictEqual(true)
+            expect(data.code).toStrictEqual(200)
+            expect(data.data).toStrictEqual(`success register id: ${sample.code} msg: ${sample.data}`)
         });
     })
 })
