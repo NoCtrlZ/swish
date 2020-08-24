51bf0dc34c31a9e8a04679a487930f0f33853fa9
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-24 19:18:41 +0900

    docker setup

--- test/client/index.test.js
+++ test/client/index.test.js
@@ -1,41 +1,38 @@
 const Client = require('hear_me_roar').default;
-const client = new Client('localhost', 3000);
+const client = new Client('server', 3000);
 
 describe('Swish Swish Integration Test', () => {
-    it('Static Get Request', () => {
-        client.get('/path').then((res) => {
-            const { status, data } = res;
+    it('Static Get Request', async () => {
+        const res = await client.get('/path').catch(e => console.log(e))
+        const { status, data } = res;
 
-            expect(status).toStrictEqual(true);
-            expect(data.code).toStrictEqual(200);
-            expect(data.data).toStrictEqual('path request');
-        });
+        expect(status).toStrictEqual(true);
+        expect(data.code).toStrictEqual(200);
+        expect(data.data).toStrictEqual('path request');
     });
 
-    it('Dynamic Get Request', () => {
-        client.get('/user/shinsaku').then((res) => {
-            const { status, data } = res;
+    it('Dynamic Get Request', async () => {
+        const res = await client.get('/user/shinsaku').catch(e => console.log(e))
+        const { status, data } = res;
 
-            expect(status).toStrictEqual(true);
-            expect(data.code).toStrictEqual(200);
-            expect(data.data).toStrictEqual('user id is shinsaku');
-        });
+        expect(status).toStrictEqual(true);
+        expect(data.code).toStrictEqual(200);
+        expect(data.data).toStrictEqual('user id is shinsaku');
     });
 
-    it('Normal Post Request', () => {
+    it('Normal Post Request', async () => {
         const sample = {
             code: 200,
             data: 'hello'
         };
 
-        client.post('/user/register', sample).then((res) => {
-            const { status, data } = res;
+        const res = await client.post('/user/register', sample)
+        const { status, data } = res;
 
-            expect(status).toStrictEqual(true);
-            expect(data.code).toStrictEqual(200);
-            expect(data.data).toStrictEqual(
-                `success register id: ${sample.code} msg: ${sample.data}`
-            );
-        });
+        expect(status).toStrictEqual(true);
+        expect(data.code).toStrictEqual(200);
+        expect(data.data).toStrictEqual(
+            `success register id: ${sample.code} msg: ${sample.data}`
+        );
     });
 });
