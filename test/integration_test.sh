4d0c7be2f4ef7dfc7280bd8ee0991b32df407986
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-24 20:41:49 +0900

    integration test handling with shell script

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -2,4 +2,19 @@
 
 cd test
 docker-compose build
-docker-compose up
+docker-compose up &
+Status=`docker-compose ps client | awk 'NR==3 { print $5 }'`
+Counter=0
+while [ "$Status" != '0' ]
+do
+    echo `docker-compose ps | awk 'NR==5 { print $1 }'`
+    Status=`docker-compose ps client | awk 'NR==3 { print $5 }'`
+    Counter=$(($Counter + 1))
+    if [ $Counter -ge 10 ] || [ "$Status" == '1' ]; then
+        echo "Jest Test Failure"
+        exit 1
+    fi
+    sleep 2
+done
+echo "Jest Test Success"
+docker-compose down
