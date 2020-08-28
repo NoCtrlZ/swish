bad75f9e53dcbfc5b061f2671b4f94ae38e28740
NoCtrlZ <phantomofrotten@gmail.com>
2020-08-28 15:09:04 +0900

    crate validater module

--- src/swish.rs
+++ src/swish.rs
@@ -6,13 +6,14 @@ use crate::http::Method;
 use crate::request::{parse, Request};
 use crate::response::Response;
 use crate::router::{handler_exec, Handler, Router};
+use crate::validater::Validater;
 
 use std::net::{TcpListener, TcpStream};
 
 pub struct Swish {
     pub router: Router,
     pub config: Config,
-    pub cors: Option<Cors>,
+    pub validater: Validater,
 }
 
 impl Swish {
@@ -20,7 +21,7 @@ impl Swish {
         Swish {
             router: Router::new(),
             config: Config::new(),
-            cors: Default::default(),
+            validater: Default::default(),
         }
     }
 
@@ -33,7 +34,7 @@ impl Swish {
     }
 
     pub fn set_cors_as(&mut self, cors: Cors) {
-        self.cors = Some(cors)
+        self.validater.set_cors(cors)
     }
 
     pub fn set_address(&mut self, address: &str) {
@@ -51,19 +52,14 @@ impl Swish {
     fn handle(&mut self, stream: &mut TcpStream) {
         let mut req = parse(stream);
         println!("{:?}", req);
-        let res = match &self.cors {
-            Some(e) => {
-                // todo should be clear the error reason by using msg, return handler
-                let (is_valid, msg) = e.validate_request(&req);
-                if is_valid {
-                    self.search(&mut req)
-                } else {
-                    handler_exec(is_unauthorized, &req)
-                }
-            }
-            None => self.search(&mut req),
-        };
-        res.send(stream);
+        let (is_valid, msg) = self.validater.validate_request(&req);
+        if is_valid {
+            let res = self.search(&mut req);
+            res.send(stream);
+        } else {
+            let res = handler_exec(is_unauthorized, &req);
+            res.send(stream);
+        }
     }
 
     fn search(&mut self, req: &mut Request) -> Response {
---
+++ src/validater.rs
@@ -0,0 +1,22 @@
+use crate::cors::Cors;
+use crate::request::Request;
+
+#[derive(Default)]
+pub struct Validater {
+    cors: Option<Cors>,
+}
+
+impl Validater {
+    pub fn set_cors(&mut self, cors: Cors) {
+        self.cors = Some(cors)
+    }
+
+    pub fn validate_request(&self, req: &Request) -> (bool, String) {
+        match &self.cors {
+            Some(cors) => {
+                return cors.validate_request(&req);
+            }
+            None => (true, "no cors option".to_string()),
+        }
+    }
+}
